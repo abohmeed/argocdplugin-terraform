d932ef90a075dc4920c6b970bae1fdd4bcdfd291
Ahmed Elfakharany <abohmeed@gmail.com>
2024-02-28 19:36:50 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "eu-west-1" # Adjust this to your preferred AWS region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+
+  # Assuming the default security group is named "default", adjust if your setup differs
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
+
+variable "db_username" {}
+variable "db_password" {}
+
+
+module "db" {
+  source     = "terraform-aws-modules/rds/aws"
+  version    = "6.4.0"
+  family     = "mysql"
+  identifier = "mydemo-mysql-db"
+
+  engine               = "mysql"
+  engine_version       = "8.0.23"
+  major_engine_version = "8.0"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+
+  db_name  = "argocddemo"
+  username = var.db_username
+  password = var.db_password
+  port     = "3306"
+
+  # Using the default VPC and subnets
+  vpc_security_group_ids = [data.aws_security_group.default.id]
+  subnet_ids             = tolist(data.aws_subnets.default.ids)
+  create_db_subnet_group = true
+  create_db_option_group = true
+  publicly_accessible    = true
+
+  # Backup, maintenance, and other settings
+  backup_retention_period = 0 # Minimize costs; consider implications for real environments
+  skip_final_snapshot     = true
+  deletion_protection     = false
+}
+
+output "db_instance_endpoint" {
+  value = module.db.db_instance_endpoint
+}
